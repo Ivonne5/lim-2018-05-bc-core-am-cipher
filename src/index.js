94b52a29d171c96911507b047dec0fd6e15be7d6
Ivonne5 <ivonne.boyer@hotmail.com>
2018-05-25 10:44:06 -0500

    evento click

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ var encrypted="";
 //especifica cada letra del texto ingresado.//
 for(i=0;i<introText.length;i++){
 //ubicacion de letra en codigo unicode (CharCodeAt) y la cambiamos a acii con formula//
-var locationLetter=((introText.toUpperCase.CharCodeAt(i)-65)+jumpPositions)%26+65;
+var locationLetter=((introText.CharCodeAt(i)-65)+jumpPositions)%26+65;
 //conversion acii a unicode//
 var textCipher=String.fromCharCode(locationLetter);
 //adicionando valor a var encrypted//
 encrypted+=textCipher;
 }
-//funcionalidad boton//
-document.getElementById('encripta').addEventListener('click',function(onload){
-document.getElementById('change').innerHTML,document.getElementById('jump').innerHTML=textCipher;
-})
+//creando evento click//
+document.getElementById("formulario").addEventListener(click,myFunction()){
+document.getElementById('change').innerHTML=encrypted;
+}
