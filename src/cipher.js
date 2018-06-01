4dc1d29631c2e892ab125e9c2871c5f3e697a278
Ivonne5 <ivonne.boyer@hotmail.com>
2018-06-01 11:25:34 -0500

    presentacion final

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,32 @@
-window.cipher(){
-// window.onload =function() {
-// document.getElementById('encripta').addEventListener('click',function(){
-// document.getElementById('intro').innerHTML,document.getElementById('jump').innerHTML=textCipher;})
-
-
-};
-}
-  // ...
+ window.cipher = {
+    encode : (offset,string) => {
+        //funcion que se ejecutara despues del click boton cifrar//
+        //elementos HTML a trabajar//
+        let resultPosition = "";
+    
+        //extraer letras del mensaje una a una//
+        for(let i=0; i<string.length; i++){
+        //buscar la posicion en unicode//
+        let unicodePosition=string.charCodeAt(i);
+        //buscar la posicion en acci//
+        let acciPosition=(unicodePosition-65+parseInt(offset))%26+65;
+        //adicionar contenido alfabetico cifrado a variable encode//
+        resultPosition+=String.fromCharCode(acciPosition);
+        }
+        return resultPosition;
+    },
+    decode : (offset,string)=>{
+        //funcion que se ejecutara despues del click boton descifrar//
+        let resultPosition="";
+        //extraer letras del mensaje una a una//
+        for(let i=0; i<string.length; i++){
+        //buscar la posicion en acci//
+        let acciPosition1=string.charCodeAt(i);
+        //buscar la posicion en unicode//
+        let unicodePosition1=(acciPosition1-parseInt(offset));
+        //adicionar contenido alfabetico descifrado a variable decode //
+        resultPosition+=String.fromCharCode(unicodePosition1);
+         }
+        return resultPosition;
+ }
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-//nombrando elementos html a trabajar//
-var introText= document.getElementById('intro').value;
-var jumpPositions= document.getElementById('jump').value;
-var encrypted="";
-//especifica cada letra del texto ingresado.//
-for(i=0;i<introText.length;i++){
-//ubicacion de letra en codigo unicode (CharCodeAt) y la cambiamos a acii con formula//
-var locationLetter=((introText.CharCodeAt(i)-65)+jumpPositions)%26+65;
-//conversion acii a unicode//
-var textCipher=String.fromCharCode(locationLetter);
-//adicionando valor a var encrypted//
-encrypted+=textCipher;
-}
-//creando evento click//
-document.getElementById("formulario").addEventListener(click,myFunction()){
-document.getElementById('change').innerHTML=encrypted;
-}
+//llamando a mis variables//
+let offset=document.getElementById("offset");
+let string=document.getElementById("string").toUpperCase();
+
+//generando evento para desencadenar la funcion cifrar//
+
+document.getElementById("encode").addEventListener("click",()=>{
+    document.getElementById("change").value=cipher.encode(offset.value, string.value);
+});
+
+//generando evento para desencadenar la funcion descifrar//
+
+document.getElementById("decode").addEventListener("click",()=>{
+document.getElementById("change").value=cipher.decode(offset.value, string.value); 
+});
