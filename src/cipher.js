8f4c90efb95ac242d0267ba4303d6dea1035b510
Ivonne5 <ivonne.boyer@hotmail.com>
2018-06-04 08:21:26 -0500

    cambio de opacidad y link de responsive

--- src/cipher.js
+++ src/cipher.js
@@ -3,31 +3,31 @@
         //funcion que se ejecutara despues del click boton cifrar//
         //elementos HTML a trabajar//
         string=string.toUpperCase();
-        let resultPosition = "";
+        let resultPosition =  " " ;
     
-        //extraer letras del mensaje una a una//
-        for(let i=0; i<string.length; i++){
-        //buscar la posicion en unicode//
-        let unicodePosition=string.charCodeAt(i);
-        //buscar la posicion en acci//
-        let acciPosition=(unicodePosition-65+parseInt(offset))%26+65;
-        //adicionar contenido alfabetico cifrado a variable encode//
-        resultPosition+=String.fromCharCode(acciPosition);
+        // extraer letras del mensaje una a una //
+        for ( let i = 0 ; i < string . length ; i ++ ) {
+        // buscar la posicion en unicode //
+        let unicodePosition = string . charCodeAt (i);
+        // buscar la posicion en acci //
+        let acciPosition = (unicodePosition - 65 + parseInt (offset)) % 26 + 65 ;
+        // adicionar contenido alfabetico cifrado una variable codificar //
+        resultPosition += String . fromCharCode (acciPosition);
         }
         return resultPosition;
     },
     decode : (offset,string)=>{
         //funcion que se ejecutara despues del click boton descifrar//
         string=string.toUpperCase();
-        let resultPosition="";
-        //extraer letras del mensaje una a una//
-        for(let i=0; i<string.length; i++){
-        //buscar la posicion en acci//
-        let acciPosition1=string.charCodeAt(i);
-        //buscar la posicion en unicode//
-        let unicodePosition1=(acciPosition1-parseInt(offset));
-        //adicionar contenido alfabetico descifrado a variable decode //
-        resultPosition+=String.fromCharCode(unicodePosition1);
+        let resultPosition = " " ;
+        // extraer letras del mensaje una a una //
+        for ( let i = 0 ; i < string . length ; i ++ ) {
+        // buscar la posicion en acci //
+        let acciPosition1 = string . charCodeAt (i);
+        // buscar la posicion en unicode //
+        let unicodePosition1 = (acciPosition1 - parseInt (offset));
+        // adicionar contenido alfabetico descifrado una decodificación variable //
+        resultPosition += String . fromCharCode (unicodePosition1);
          }
         return resultPosition;
  }
